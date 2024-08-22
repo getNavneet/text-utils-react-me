efe9b7a492e2eacaf7790b25c4658b043e48dd70
Navneet <navneetkumar.html@gmail.com>
2024-08-22 21:31:53 +0530

    make header sticky

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { NavLink ,Link} from 'react-router-dom';
-import { useLocation } from 'react-router-dom';
 function Header() {
   return (
     <>
-     <nav class="flex w-screen bg-red-700 items-center justify-between px-4 py-2">
+     <nav class="sticky top-0 z-10 bg-lime-300  flex w-screen  items-center justify-between px-4 py-2">
 <div className='mx-7 bg-slate-50'>
   <ul class="flex space-x-7 text-green bg-amber-200">
     <li><NavLink  to="/" class="hover:text-red-600">Home</NavLink></li>
     <li><NavLink  to="/about" class="hover:text-red-600">About</NavLink></li>
-    <li><Link  to="/#guide" class="hover:text-red-600">Guide</Link></li>
-    <li><Link  to="#" class="hover:text-red-600">Explore</Link></li>
+    <li><NavLink  to="/#guide" class="hover:text-red-600">Guide</NavLink></li>
+    <li><NavLink  to="#" class="hover:text-red-600">Explore</NavLink></li>
   </ul>
   </div> 
 </nav>
