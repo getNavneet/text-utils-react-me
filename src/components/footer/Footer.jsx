76e4fabc0e2ff6a991f56b90ed9d6346f79f6e47
Navneet <navneetkumar.html@gmail.com>
2024-08-26 17:28:49 +0530

    change footer design ,add social links

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,27 +1,48 @@
 import React from 'react';
-import { NavLink ,Link} from 'react-router-dom';
+import { NavLink} from 'react-router-dom';
+import github from "../../assets/github.svg"
+import insta from "../../assets/instagram.svg"
+import twitter from "../../assets/twitter.svg"
+import whatsapp from "../../assets/whatsapp.svg"
 
 function Footer() {
   return (
     <>
-        <footer className='w-full text-white relative'>
-<div className='bg-[#3D005E] flex justify-between items-center '>
-<ul class="flex text-green flex-col pl-14 sm:pl-6 p-6 space-y-4 sm:space-y-2">
-    <li><NavLink  to="/" class="hover:text-red-600">&rarr; Home</NavLink></li>
-    <li><NavLink  to="/about" class="hover:text-red-600">&rarr; About</NavLink></li>
-    <li><NavLink  to="/#guide" class="hover:text-red-600">&rarr; Guide</NavLink></li>
-    <li><NavLink  to="#" class="hover:text-red-600">&rarr; Explore</NavLink></li>
+        <footer className='w-full text-white relative '>
+<div className='bg-cyan-900 flex flex-row justify-between  items-center'>
+<ul class="flex  flex-col ml-7 m-6 lg:ml-44 space-y-4 sm:space-y-2 ">
+    <li><NavLink  to="/" >&rarr; Home</NavLink></li>
+    <li><NavLink  to="/about" >&rarr; About</NavLink></li>
+    <li><NavLink  to="/#guide" >&rarr; Guide</NavLink></li>
+    <li><NavLink  to="#" >&rarr; Explore</NavLink></li>
   </ul>
-  <address className='pr-14 sm:pr-10'>
+  <address className='m-2 mr-5 lg:mr-28  '>
     campwell road , 226003 <br></br>
 lucknow ,Uttar Pradesh <br></br>
-<span>+91 730-715-9865</span>
+<span><a href="tel:+917307159865">+91 730-715-9865</a></span>
   </address>
-
 </div>
-    <div className='min-h-12 bg-blue-950 inset-0 flex justify-center items-center'>
-        <h1 className="text-center">
-            made with ❤ by <a href='https://navneet.website/'>Navneet</a>
+
+<div className="flex flex-row justify-center items-center space-x-5 lg:space-x-8 p-4 bg-cyan-900  ">
+          <a href="https://github.com/getNavneet" target="_blank" rel="noreferrer">
+            <img src={github} alt="linkedin" className="w-10 lg:w-13 " />
+          </a>
+          <a href="https://x.com/get2Navneet" target="_blank" rel="noreferrer">
+            <img src={twitter} alt="github" className="w-11 lg:w-14" />
+          </a>
+          <a href="https://www.instagram.com/bittu_boss__01_/" target="_blank" rel="noreferrer">
+            <img src={insta} alt="instagram" className="w-9 lg:w-11" />
+          </a>
+          <a href="https://wa.me/+917307159865?text=hello%20Navneet" target="_blank" rel="noreferrer">
+            <img src={whatsapp} alt="twitter" className="w-9 lg:w-11" />
+          </a>
+          
+        </div>
+
+    <div className='min-h-12  flex flex-col bg-cyan-950 inset-0  justify-center items-center'>
+    <p className='p-3'>&copy; 2024 textCraftz</p>
+        <h1 className="text-center pb-3">
+            made with ❤ by <a href='https://navneet.website/'><span className=''>Navneet</span></a>
         </h1>
     </div>
 </footer>
